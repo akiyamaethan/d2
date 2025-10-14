0e116d42d6e698cb70d2c17c4b4f5c5d28f52989
Ethan Akiyama <ekakiyam@ucsc.edu>
2025-10-14 03:53:57 +0000

    step 2 complete: implemented drawing and clearing on the canvas. TODO: fix cursor offset

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,41 @@ document.body.innerHTML = `
     <h1>Ethan's Artistic Space</h1>
     <canvas 
       id="myCanvas"
-      width="20"
-      height="20" 
     ></canvas>
-
+    <br/>
+    <button
+      id="clearBtn"
+    >Clear</button>
   </center>
 `;
+
+const clearButton = document.getElementById("clearBtn") as HTMLButtonElement;
+const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (mousePosition) => {
+  cursor.active = true;
+  cursor.x = mousePosition.offsetX;
+  cursor.y = mousePosition.offsetY;
+});
+
+canvas.addEventListener("mousemove", (mousePosition) => {
+  if (cursor.active) {
+    ctx?.beginPath();
+    ctx?.moveTo(cursor.x, cursor.y);
+    ctx?.lineTo(mousePosition.offsetX, mousePosition.offsetY);
+    ctx?.stroke();
+    cursor.x = mousePosition.offsetX;
+    cursor.y = mousePosition.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+clearButton.addEventListener("click", () => {
+  ctx?.clearRect(0, 0, canvas.width, canvas.height);
+});
